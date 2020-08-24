afc9010fed5c74ffeeb1d9004936e7ab7ed383e6
wc <1002420995@qq.com>
2020-08-24 21:23:19 +0800

    register center init

---
+++ src/test/java/com/wei/chong/root/weichongeureka/WeiChongEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wei.chong.root.weichongeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeiChongEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
